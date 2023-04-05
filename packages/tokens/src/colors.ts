3e1596a1c05a9a6c2b49ef093175abb750e8f341
Felipe Gangrel <felipefrog@gmail.ccom>
2023-04-05 14:34:24 -0300

    Creates @acme/tokens package

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,25 @@
+export const lightColors = {
+  primary100: "hsl(260, 50%, 50%)",
+  primary200: "hsl(260, 50%, 40%)",
+  primary300: "hsl(260, 50%, 30%)",
+  accent100: "hsl(194, 60%, 50%)",
+  accent200: "hsl(194, 60%, 40%)",
+  accent300: "hsl(194, 60%, 30%)",
+  text100: "hsl(260, 10%, 20%)",
+  text200: "hsl(260, 10%, 10%)",
+  surface100: "hsl(260, 20%, 90%)",
+  surface200: "hsl(260, 20%, 80%)",
+} as const;
+
+export const darkColors = {
+  primary100: "hsl(260, 40%, 50%)",
+  primary200: "hsl(260, 40%, 40%)",
+  primary300: "hsl(260, 40%, 30%)",
+  accent100: "hsl(194, 50%, 50%)",
+  accent200: "hsl(194, 50%, 40%)",
+  accent300: "hsl(194, 50%, 30%)",
+  text100: "hsl(260, 10%, 90%)",
+  text200: "hsl(260, 10%, 80%)",
+  surface100: "hsl(260, 20%, 10%)",
+  surface200: "hsl(260, 20%, 20%)",
+} as const;
---
+++ packages/tokens/src/font-sizes.ts
@@ -0,0 +1,7 @@
+export const fontSizes = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+} as const;
